89b0e0a4448d2eca66636c2495c7a2f4a5cc6493
aheyman11 <aheyman11@gmail.com>
2016-06-12 22:36:08 -0400

    Basic setup for backend flask server

---
+++ server/server.py
@@ -0,0 +1,9 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+@app.route('/')
+def process_text():
+    return 'Response from backend'
