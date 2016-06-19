e85640d0babc562fdb30eb43f679fb73458fdd63
Biggie Emmanuel <biggie@skillshare.com>
2016-06-19 17:22:46 -0400

    add shebang

--- server/server.py
+++ server/server.py
@@ -1,9 +1,15 @@
+#!/usr/bin/env python
+
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
 app = Flask(__name__)
 app.config.from_object(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def process_text():
     return 'Response from backend'
+
+@app.route('/solutions', methods=['GET'])
+def solutions():
+    return 'answers' 
