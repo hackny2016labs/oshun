2a4d859b7d01844fb0208b71ed628844ac85a2fe
aheyman11 <aheyman11@gmail.com>
2016-07-03 22:20:37 -0400

    Added handle to interact with database

--- server/server.py
+++ server/server.py
@@ -3,8 +3,17 @@
 from flask import Flask, request, session, g, redirect, url_for, abort, \
      render_template, flash
 
+from pymongo import MongoClient
+
+# returns a handle to the test db
+def connect_db():
+    connection = MongoClient() # defaults to localhost port 27017
+    db = connection['test-db']
+    return db
+
 app = Flask(__name__)
 app.config.from_object(__name__)
+db = connect_db()
 
 @app.route('/', methods=['POST'])
 def process_text():
